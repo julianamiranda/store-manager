e5aa9afc8c2c5c0ea25437155cdb1681e7dd1892
juliana <juliana.ms@live.com>
2022-08-11 23:02:38 -0300

    :sparkles: [TESTS] ProductsController - getById

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -48,4 +48,48 @@ describe('Controller - Products', () => {
       });
     });
   });
+
+  describe('#getById', () => {
+    describe('quando existe o produto com o id solicitado', () => {
+      const response = {};
+      const request = {};
+      before(() => {
+        request.params = 1;
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+        sinon.stub(productsService, 'getById').resolves(data.allProductsResponse[0])
+      });
+      after(() => productsService.getById.restore());
+
+      it('o status retornado é 200', async () => {
+        await productsController.getById(request, response);
+        expect(response.status.calledWith(200)).to.be.equal(true);
+      });
+      it('retorna um objeto com os dados', async () => {
+        await productsController.getById(request, response);
+        expect(response.json.calledWith(data.allProductsResponse[0])).to.be.equal(true);
+      });
+    });
+
+    describe('quando não existe um produto com o id solicitado', () => {
+      const response = {};
+      const request = {};
+      before(() => {
+        request.params = 21;
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+        sinon.stub(productsService, 'getById').resolves(null)
+      });
+      after(() => productsService.getById.restore());
+
+      it('o status retornado 404', async () => {
+        await productsController.getById(request, response);
+        expect(response.status.calledWith(404)).to.be.equal(true);
+      });
+      it('retorna uma mensagem', async () => {
+        await productsController.getById(request, response);
+        expect(response.json.calledWith({ message: 'Product not found' })).to.be.equal(true);
+      });
+    });
+  });
 });

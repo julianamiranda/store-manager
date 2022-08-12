97262d50cc4f6b67f2118c9594aca94d9601bab0
juliana <juliana.ms@live.com>
2022-08-11 21:15:40 -0300

    :memo: alteração no productsController

--- controllers/productsContoller.js
+++ controllers/productsContoller.js
@@ -2,13 +2,13 @@ const products = require('../services/productsService');
 
 const getAll = async (_req, res) => {
   const result = await products.getAll();
-  if (result === null) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message: 'Product not found' });
   return res.status(200).json(result);
 };
 const getById = async (req, res) => {
   const id = Number(req.params.id);
   const result = await products.getById(id);
-  if (result === null) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message: 'Product not found' });
   return res.status(200).json(result);
 };
 

2f57c399d26fe6897a87045959097efbc49bc2f0
juliana <juliana.ms@live.com>
2022-08-12 20:39:24 -0300

    :memo: fix lint

--- controllers/productsContoller.js
+++ controllers/productsContoller.js
@@ -1,14 +1,16 @@
 const products = require('../services/productsService');
 
+const message = 'Product not found';
+
 const getAll = async (_req, res) => {
   const result = await products.getAll();
-  if (!result) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message });
   return res.status(200).json(result);
 };
 const getById = async (req, res) => {
   const id = Number(req.params.id);
   const result = await products.getById(id);
-  if (!result) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message });
   return res.status(200).json(result);
 };
 
@@ -22,21 +24,21 @@ const update = async (req, res) => {
   const id = Number(req.params.id);
   const { name } = req.body;
   const result = await products.update(name, id);
-  if (!result) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message });
   return res.status(200).json(result);
 };
 
 const remove = async (req, res) => {
   const id = Number(req.params.id);
   const result = await products.remove(id);
-  if (!result) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message });
   return res.sendStatus(204);
 };
 
 const search = async (req, res) => {
   const { q } = req.query;
   const result = await products.search(`%${q}%`);
-  if (!result) return res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message });
   return res.status(200).json(result);
 };
 
